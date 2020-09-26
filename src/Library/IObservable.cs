7c5880730dc761b00af56abac20dde5a8b323585
Felipe <felipe.cabrera@correo.ucu.edu.uy>
2020-09-25 21:54:31 -0300

    interface

---
+++ src/Library/IObservable.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Observer
+{
+    public interface IObservable<T>
+    {
+        void Subscribe(IObserver<T> observer);
+
+        void Unsubscribe(IObserver<T> observer);
+    }
+
+}
